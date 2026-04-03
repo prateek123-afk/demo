76de55be2808f3bbd0b5de34a0749374f02241b9
prateek <kalapanshu@gmail.com>
2026-04-03 16:43:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const compressRoute = require("./routes/compress");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "frontend"))); 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
-});
-app.get("/", (req, res) => {
-  res.send("Backend is running ");
-});
-
 app.use("/api", compressRoute);
 
 app.listen(5000, () => {
